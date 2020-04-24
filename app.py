f59b9059762bf041575d44d3465fa89bcd19e3b5
arihant <arihant.mrn@gmail.com>
2020-04-24 23:54:44 +1000

    homepage done - working on assessment

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
+
+@app.route('/assessment')
+def assessment():
+    return render_template("assessment.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
