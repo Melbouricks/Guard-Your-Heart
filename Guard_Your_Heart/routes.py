ea650d083bd22cf92de88d63329f0a258bf702fb
dmen0009@student.monash.edu <dmen0009@student.monash.edu>
2020-05-13 23:39:33 +1000

    session done

--- Guard_Your_Heart/routes.py
+++ Guard_Your_Heart/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, session, redirect
 from Guard_Your_Heart import app
 from Guard_Your_Heart.utils import Utill 
 
@@ -7,60 +7,83 @@ from Guard_Your_Heart.utils import Utill
 def initlogin():
     return render_template("initlogin.html")
 
-@app.route('/home',methods=['POST'])
-def index():
+@app.route('/check-login',methods=['POST'])
+def checkLogin():
     if request.method == 'POST':
-        if request.form.get("password") == "29958563":
-            return render_template("index.html", route="index")
-            
+        session.permanent = True
+        session['user'] = request.form.get('username')
+        if request.form.get("password") == "teamb18":
+            return redirect(url_for("index"))
+    else:
+        if "user" in session:
+            return redirect(url_for("index"))
+
     return render_template('initlogin.html')
 
+@app.route('/home')
+def index():
+    if 'user' in session:
+        return render_template("index.html", route="index")
+    else:
+        return redirect(url_for('initlogin'))
 
 @app.route('/assessment')
 def assessment():
-    return render_template("assessment.html", route="assessment")
+    if 'user' in session:
+        return render_template("assessment.html", route="assessment")
+    else:
+        return redirect(url_for('initlogin'))
 
 
 @app.route('/option')
 def option():
-    list_of_headings = Utill.metData()
-    print(list_of_headings)
-    return render_template("option.html", activity_data=list_of_headings, route="option")
-
-
-@app.route('/results', methods=['POST'])
-def results():
-    if request.method == 'POST':
-
-        # Better way to access data than request.form['age']
-        # to avoid getting an exception if null value is sent.
-        # print(request.form.get('age'))
-        data_sample = Utill.get_data(request)
-        print(data_sample)
-        # activity_data = data_sample.pop('activityname')
-        # abc = get_activity_data(activity_data)
-        result = Utill.predict_data(data_sample)
-        print("\nChances of having CVD:", result)
-        data_sample['res'] = result
-        data_sample['indicator'] = [int(request.form.get("chol")),int(request.form.get("sugarradio"))]
-        print(data_sample['indicator'][0])
-        print(data_sample['indicator'][1])
-        return render_template('results.html', data=data_sample, route="result")
+    if 'user' in session:
+        list_of_headings = Utill.metData()
+        print(list_of_headings)
+        return render_template("option.html", activity_data=list_of_headings, route="option")
     else:
-        return render_template('home.html', route="index")
+        return redirect(url_for('initlogin'))
+
+
+@app.route('/results', methods=['POST','GET'])
+def results():
+    if 'user' in session:
+        if request.method == 'POST':
+            data_sample = Utill.get_data(request)
+            result = Utill.predict_data(data_sample)
+            data_sample['res'] = result
+            data_sample['indicator'] = [int(request.form.get("chol")),int(request.form.get("sugarradio"))]
+
+            return render_template('results.html', data=data_sample, route="result")
+        else:
+            return render_template('home.html', route="index")
+    else:
+        return redirect(url_for('initlogin'))
 
 @app.route('/diet')
 def diet():
-    return render_template("diet.html", route="diet")
+    if 'user' in session:
+        return render_template("diet.html", route="diet")
+    else:
+        return redirect(url_for('initlogin'))
     
 @app.route('/PA')
 def PA():
-    return render_template("PA.html", route="PA")
+    if 'user' in session:
+        return render_template("PA.html", route="PA")
+    else:
+        return redirect(url_for('intilogin'))
     
 @app.route('/cardiovasculardisease')
 def cardiovasculardisease():
-    return render_template("cardiovasculardisease.html", route="cardiovasculardisease")
+    if 'user' in session:
+        return render_template("cardiovasculardisease.html", route="cardiovasculardisease")
+    else:
+        return redirect(url_for('initlogin'))
 
 @app.route('/test',methods=['POST'])
 def test():
-    return render_template("test.html")
+    if 'user' in session:
+        return render_template("test.html")
+    else:
+        return redirect(url_for('initlogin'))
